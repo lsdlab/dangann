ad189c8a4d4ed8c8f347ae1cf2618145849b40f4
Chen Jian <lsdlab@icloud.com>
2017-05-16 16:54:28 +0800

    update deploy conf

--- conf/production/settings.py
+++ conf/production/settings.py
@@ -189,11 +189,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 MEDIA_ROOT = 'media/'
 MEDIA_URL = '/media/'
