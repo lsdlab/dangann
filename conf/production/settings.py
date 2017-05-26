a8de2687cc80d7977122e3a9276b12e6b8d498b4
Chen Jian <lsdlab@icloud.com>
2017-05-27 01:50:40 +0800

    update

--- conf/production/settings.py
+++ conf/production/settings.py
@@ -193,7 +193,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/root/dangann/static'
+STATIC_ROOT = '/home/ubuntu/dangann/static'
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "static"),
